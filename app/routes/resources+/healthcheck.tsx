40c33112c2c3080cedabaaa2cdd9fa45b9593be1
Alexandru Bereghici <alexandru.brg@gmail.com>
2023-07-18 11:22:43 +0300

    update healthcheck

--- app/routes/resources+/healthcheck.tsx
+++ app/routes/resources+/healthcheck.tsx
@@ -4,18 +4,18 @@ import { type DataFunctionArgs } from '@remix-run/node'
 import { prisma } from '~/utils/db.server.ts'
 
 export async function loader({ request }: DataFunctionArgs) {
-	const host =
-		request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
+	// const host =
+	// 	request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
 
 	try {
-		const url = new URL('/', `http://${host}`)
+		// const url = new URL('/', `http://${host}`)
 		// if we can connect to the database and make a simple query
 		// and make a HEAD request to ourselves, then we're good.
 		await Promise.all([
 			prisma.user.count(),
-			fetch(url.toString(), { method: 'HEAD' }).then(r => {
-				if (!r.ok) return Promise.reject(r)
-			}),
+			// fetch(url.toString(), { method: 'HEAD' }).then(r => {
+			// 	if (!r.ok) return Promise.reject(r)
+			// }),
 		])
 		return new Response('OK')
 	} catch (error: unknown) {
