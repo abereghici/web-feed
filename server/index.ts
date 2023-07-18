829d6d7a0c3a32abc58c7489fd6746551cf33e8b
Alexandru Bereghici <alexandru.brg@gmail.com>
2023-07-18 13:01:41 +0300

    csp fix for analyics

--- server/index.ts
+++ server/index.ts
@@ -97,6 +97,7 @@ app.use(
 				'connect-src': [
 					MODE === 'development' ? 'ws:' : null,
 					process.env.SENTRY_DSN ? '*.ingest.sentry.io' : null,
+					process.env.GA_TRACKING_ID ? 'www.google-analytics.com' : null,
 					"'self'",
 				].filter(Boolean),
 				'font-src': ["'self'"],
